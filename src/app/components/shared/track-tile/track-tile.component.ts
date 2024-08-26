e3fdd2b47db73594defcd3e3532224f4247fdc7f
simple17 <macro.poetry@gmail.com>
2024-08-26 11:18:29 +0200

    add mat-button import to track tile

--- src/app/components/shared/track-tile/track-tile.component.ts
+++ src/app/components/shared/track-tile/track-tile.component.ts
@@ -1,11 +1,12 @@
 import { Component, EventEmitter, input, Output } from '@angular/core';
 import { UserTrack } from '../../../interfaces/UserTrack';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-track-tile',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './track-tile.component.html',
   styleUrl: './track-tile.component.scss'
 })
